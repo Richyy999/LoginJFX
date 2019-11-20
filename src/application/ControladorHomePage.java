20ee70a9fc41159f660fec819887f152fb5144c7
Ricardo Bordería Pi <rychyy999@gmail.com>
2019-11-20 12:21:47 +0100

    retoque menu

--- src/application/ControladorHomePage.java
+++ src/application/ControladorHomePage.java
@@ -26,7 +26,7 @@ public class ControladorHomePage {
 
 	@FXML
 	private ImageView img;
-	
+
 	private boolean mostrado;
 
 	public void initialize() {
@@ -40,7 +40,10 @@ public class ControladorHomePage {
 		});
 
 		img.setOnMouseClicked(event -> {
-			mostrarMenu();
+			if (mostrado)
+				esconderMenu();
+			else
+				mostrarMenu();
 		});
 	}
 
@@ -58,58 +61,6 @@ public class ControladorHomePage {
 		}
 	}
 
-	private void animacionHome() {
-		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), aPan4);
-		fadeTransition.setFromValue(1);
-		fadeTransition.setToValue(0);
-		fadeTransition.play();
-
-		fadeTransition.setOnFinished(event -> {
-
-			FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(3), aPan3);
-			fadeTransition1.setFromValue(1);
-			fadeTransition1.setToValue(0);
-			fadeTransition1.play();
-
-			fadeTransition1.setOnFinished(event1 -> {
-				FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(3), aPan2);
-				fadeTransition2.setFromValue(1);
-				fadeTransition2.setToValue(0);
-				fadeTransition2.play();
-
-				fadeTransition2.setOnFinished(event2 -> {
-
-					FadeTransition fadeTransition0 = new FadeTransition(Duration.seconds(3), aPan1);
-					fadeTransition0.setFromValue(0);
-					fadeTransition0.setToValue(1);
-					fadeTransition0.play();
-
-					fadeTransition0.setOnFinished(event3 -> {
-
-						FadeTransition fadeTransition11 = new FadeTransition(Duration.seconds(3), aPan3);
-
-						fadeTransition11.setFromValue(0);
-						fadeTransition11.setToValue(1);
-						fadeTransition11.play();
-
-						fadeTransition11.setOnFinished(event4 -> {
-
-							FadeTransition fadeTransition22 = new FadeTransition(Duration.seconds(3), aPan4);
-
-							fadeTransition22.setFromValue(0);
-							fadeTransition22.setToValue(1);
-							fadeTransition22.play();
-
-							fadeTransition22.setOnFinished(event5 -> {
-								animacionHome();
-							});
-						});
-					});
-				});
-			});
-		});
-	}
-
 	private void mostrarMenu() {
 		if (!mostrado) {
 			opacityPane.setVisible(true);
@@ -123,5 +74,51 @@ public class ControladorHomePage {
 			tt.play();
 			mostrado = true;
 		}
+	}
+
+	private void animacionHome() {
+		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), aPan4);
+		fadeTransition.setFromValue(1);
+		fadeTransition.setToValue(0);
+		fadeTransition.play();
+
+		fadeTransition.setOnFinished(event -> {
+			FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(3), aPan3);
+			fadeTransition1.setFromValue(1);
+			fadeTransition1.setToValue(0);
+			fadeTransition1.play();
+
+			fadeTransition1.setOnFinished(event1 -> {
+				FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(3), aPan2);
+				fadeTransition2.setFromValue(1);
+				fadeTransition2.setToValue(0);
+				fadeTransition2.play();
+
+				fadeTransition2.setOnFinished(event2 -> {
+					FadeTransition fadeTransition0 = new FadeTransition(Duration.seconds(3), aPan1);
+					fadeTransition0.setFromValue(0);
+					fadeTransition0.setToValue(1);
+					fadeTransition0.play();
+
+					fadeTransition0.setOnFinished(event3 -> {
+						FadeTransition fadeTransition11 = new FadeTransition(Duration.seconds(3), aPan3);
+						fadeTransition11.setFromValue(0);
+						fadeTransition11.setToValue(1);
+						fadeTransition11.play();
+
+						fadeTransition11.setOnFinished(event4 -> {
+							FadeTransition fadeTransition22 = new FadeTransition(Duration.seconds(3), aPan4);
+							fadeTransition22.setFromValue(0);
+							fadeTransition22.setToValue(1);
+							fadeTransition22.play();
+
+							fadeTransition22.setOnFinished(event5 -> {
+								animacionHome();
+							});
+						});
+					});
+				});
+			});
+		});
 	}
 }
