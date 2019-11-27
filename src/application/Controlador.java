1c746de0ea61fddb0eddcb3be609429aea0603cb
Ricardo Bordería Pi <rychyy999@gmail.com>
2019-11-27 18:32:43 +0100

    crear splash

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -4,8 +4,6 @@ import javafx.animation.FadeTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -42,23 +40,8 @@ public class Controlador {
 		System.exit(0);
 	}
 
-	private void crearLogin() {
-		Stage primaryStage = new Stage();
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
-			AnchorPane pane = (AnchorPane) loader.load();
-			Scene scene = new Scene(pane, 1150, 686);
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	public void initialize() {
 		transiciones();
-		backgroundAnimation();
 	}
 
 	private void transiciones() {
@@ -103,61 +86,7 @@ public class Controlador {
 							ft.play();
 							ft.setOnFinished(event5 -> {
 								Stage sala = (Stage) lblNombre.getScene().getWindow();
-								crearLogin();
 								sala.close();
-							});
-						});
-					});
-				});
-			});
-		});
-	}
-
-	private void backgroundAnimation() {
-		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), panel4);
-		fadeTransition.setFromValue(1);
-		fadeTransition.setToValue(0);
-		fadeTransition.play();
-
-		fadeTransition.setOnFinished(event -> {
-
-			FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(3), panel3);
-			fadeTransition1.setFromValue(1);
-			fadeTransition1.setToValue(0);
-			fadeTransition1.play();
-
-			fadeTransition1.setOnFinished(event1 -> {
-				FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(3), panel2);
-				fadeTransition2.setFromValue(1);
-				fadeTransition2.setToValue(0);
-				fadeTransition2.play();
-
-				fadeTransition2.setOnFinished(event2 -> {
-
-					FadeTransition fadeTransition0 = new FadeTransition(Duration.seconds(3), panel2);
-					fadeTransition0.setFromValue(0);
-					fadeTransition0.setToValue(1);
-					fadeTransition0.play();
-
-					fadeTransition0.setOnFinished(event3 -> {
-
-						FadeTransition fadeTransition11 = new FadeTransition(Duration.seconds(3), panel3);
-
-						fadeTransition11.setFromValue(0);
-						fadeTransition11.setToValue(1);
-						fadeTransition11.play();
-
-						fadeTransition11.setOnFinished(event4 -> {
-
-							FadeTransition fadeTransition22 = new FadeTransition(Duration.seconds(3), panel4);
-
-							fadeTransition22.setFromValue(0);
-							fadeTransition22.setToValue(1);
-							fadeTransition22.play();
-
-							fadeTransition22.setOnFinished(event5 -> {
-
-								backgroundAnimation();
 							});
 						});
 					});
